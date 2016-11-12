ec4d3790aca400d7fbf144645310d3c058285761
Daniel Walldorf <entekmedia@gmail.com>
2016-11-12 14:12:19 +0100

    Update main.go

--- cmd/demoparser-worker/main.go
+++ cmd/demoparser-worker/main.go
@@ -28,7 +28,7 @@ type DemoParsingResult struct {
 
 func main() {
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
